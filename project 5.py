f16790470572c2169fc2d6fed9af1293b236ffc3
nicolenelson10 <nicolenelson2028@cs.sjcetpalai.ac.in>
2024-10-08 14:52:18 +0530

    Update project 5.py

--- project 5.py
+++ project 5.py
@@ -1,1 +1,6 @@
+'''
+Author:Nicole Nelson
+date:05-10-2024
+version: 1.0
+'''
 print("twingle,twingle little star\n\tHow I wonder what you are!\n\t\t)Up above the world so high\n\t\tLike a diamond in the sky\nTwinkle, twinkle, little star,\n\tHow I wonder what you are")
